4729cbcb3f3c2a73935044d24c95ddfcad76b04f
Roman <r_af_95@hotmail.com>
2020-03-03 20:46:41 -0300

    a

--- callback.js
+++ callback.js
@@ -2,6 +2,14 @@ function siguiente(x, done) {
   done(x + 1)
 }
 
+function doble(x, done) {
+  done(x * 2)
+}
+
+function dobledobleDelSiguiente(x, done) {
+  done(x * 2)
+}
+
 siguiente(2, resultado => { 
   console.log('Siguiente de 8 =>', resultado)
 })
